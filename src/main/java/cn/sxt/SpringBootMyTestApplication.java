f9bbb2430af04ea21d4f448e033f3fc32df8ba0f
mengshunchao <2467876947@qq.com>
2019-01-05 19:50:39 +0800

    初次提交

---
+++ src/main/java/cn/sxt/SpringBootMyTestApplication.java
@@ -0,0 +1,14 @@
+package cn.sxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyTestApplication.class, args);
+	}
+
+}
+
